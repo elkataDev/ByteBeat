98036a6084998a2f75a53976408ed805291182a1
Antonio Espinola Ramirez <antonioespinola344@gmail.com>
2026-06-14 12:20:53 +0200

    Inicializo el backend con spring boot + dependencias

---
+++ bytebeat-backend/src/main/java/com/github/elkataDev/bytebeat/BytebeatApplication.java
@@ -0,0 +1,13 @@
+package com.github.elkataDev.bytebeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytebeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytebeatApplication.class, args);
+	}
+
+}
---
+++ bytebeat-backend/src/test/java/com/github/elkataDev/bytebeat/BytebeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elkataDev.bytebeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytebeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
