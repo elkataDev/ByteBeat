9e5d8ba26a0a32431a7362a90618ffbf144deb29
Antonio Espinola Ramirez <antonioespinola344@gmail.com>
2026-06-17 16:07:38 +0200

    Logica de negocio PlaylistService

--- bytebeat-backend/src/main/java/com/github/elkataDev/bytebeat/repository/UserRepository.java
+++ bytebeat-backend/src/main/java/com/github/elkataDev/bytebeat/repository/UserRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.elkataDev.bytebeat.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
-    // Genera un 'SELECT * FROM artists WHERE name = ?'
+
+    // Genera un 'SELECT * FROM users WHERE username = ?'
     // Usamos Optional por si alguien busca un usuario que NO existe en la base de
     // datos (ej: "pepito garcia")
     Optional<User> findByUsername(String username);
+
+    // Genera un 'SELECT * FROM users WHERE email = ?'
+    // Usamos Optional por si alguien busca un usuario que NO existe en la base de
+    // datos (ej: "pepitogarcia@gmail.com")
+    Optional<User> findByEmail(String email);
 }
--- bytebeat-backend/src/main/java/com/github/elkataDev/bytebeat/service/PlaylistService.java
+++ bytebeat-backend/src/main/java/com/github/elkataDev/bytebeat/service/PlaylistService.java
@@ -41,41 +41,9 @@ public class PlaylistService {
     }
 
     /**
-     * Logica de Negocio
-     * Añadir un colaborador a una playlist
-     * Solo el propietario (owner) puede invitar a otros usuario como colaboradores
-     */
-
-    public Playlist addCollaborator(Long playlistId, Long newCollaboratorId, Long ownerId) {
-
-        // Buscamos la playlist donde se va a añadir el colaborador
-        Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(() -> new RuntimeException("Error: Playlist no encontrada"));
-
-        // Validamos que quien invita es el dueño de la playlist
-        if (!playlist.getOwner().getId().equals(ownerId)) {
-            throw new RuntimeException("Error: solo el propietario puede añadir colaboradores");
-        }
-
-        // Validadamos que el colaborador/usuario que queremos añadir exista
-        User newCollaborator = userRepository.findById(newCollaboratorId).orElseThrow(() -> new RuntimeException("El usuario que quieres añadir no existe"));
-
-        // Evitar que el dueño se añada a sí mismo como colaborador
-        if (playlist.getOwner().getId().equals(ownerId)) {
-            throw new RuntimeException("Error: El dueño no se puede añadir a si mismo, ya tiene el control de la playlist");
-        }
-
-        // Si ya es colaborador, no lo añadimos
-        if (!playlist.getCollaborators().contains(newCollaborator)) {
-            playlist.getCollaborators().add(newCollaborator); // Se añade el nuevo colaborador
-        }
-
-        // Guardamos/actualizamos la BD
-        return playlistRepository.save(playlist);
-    }
-
-    /**
      * Validacion de Permisos
-     * Comprueba si un usuario tiene permisos para editar una playlist
+     * Comprueba si un usuario tiene permisos para editar una playlist 
+     * Si es owner o colaborador --> True
      */
 
     public boolean hasWritePermission(Long playlistId, Long userId){
@@ -90,6 +58,130 @@ public class PlaylistService {
 
         // stream y 'anyMatch' devuelve true en el primer colaborador que coincida con el userId.
         return playlist.getCollaborators().stream().anyMatch(collaborator -> collaborator.getId().equals(userId));
+    }
+
+    /**
+     * Logica de Negocio
+     * Añadir un colaborador por ID a una playlist
+     * Solo el propietario (owner) puede invitar a otros usuario como colaboradores
+     */
+
+    public Playlist addCollaboratorWithId(Long playlistId, Long newCollaboratorId, Long ownerId) {
+
+        // Buscamos la playlist donde se va a añadir el colaborador
+        Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(() -> new RuntimeException("Error: Playlist no encontrada"));
+
+        // Validamos que quien invita es el dueño de la playlist
+        if (!playlist.getOwner().getId().equals(ownerId)) {
+            throw new RuntimeException("Error: solo el propietario puede añadir colaboradores");
+        }
+
+        // Validadamos que el colaborador/usuario que queremos añadir exista
+        User newCollaborator = userRepository.findById(newCollaboratorId).orElseThrow(() -> new RuntimeException("El usuario que quieres añadir no existe"));
+
+        // Evitar que el dueño se añada a sí mismo como colaborador
+        if (playlist.getOwner().getId().equals(newCollaboratorId)) {
+            throw new RuntimeException("Error: El dueño no se puede añadir a si mismo, ya tiene el control de la playlist");
+        }
+
+        // Si ya es colaborador, no lo añadimos
+        if (!playlist.getCollaborators().contains(newCollaborator)) {
+            playlist.getCollaborators().add(newCollaborator); // Se añade el nuevo colaborador
+        }
+
+        // Guardamos/actualizamos la BD
+        return playlistRepository.save(playlist);
+    }
+
+    /**
+     * Logica de negocio
+     * Añadir colaborador por email a una playlist
+     * Solo el propietario (owner) puede invitar a otros usuario como colaboradores
+     */
+
+        public Playlist addCollaboratorWithEmail(Long playlistId, String newCollaboratorEmail, String ownerEmail) {
+
+        // Buscamos la playlist donde se va a añadir el colaborador
+        Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(() -> new RuntimeException("Error: Playlist no encontrada"));
+
+        // Validamos que quien invita es el dueño de la playlist
+        if (!playlist.getOwner().getEmail().equals(ownerEmail)) {
+            throw new RuntimeException("Error: solo el propietario puede añadir colaboradores");
+        }
+
+        // Validadamos que el colaborador/usuario que queremos añadir exista
+        User newCollaborator = userRepository.findByEmail(newCollaboratorEmail).orElseThrow(() -> new RuntimeException("El usuario que quieres añadir no existe"));
+
+        // Evitar que el dueño se añada a sí mismo como colaborador
+        if (playlist.getOwner().getEmail().equals(newCollaboratorEmail)) {
+            throw new RuntimeException("Error: El dueño no se puede añadir a si mismo, ya tiene el control de la playlist");
+        }
+
+        // Si no es colaborador de la playlist lo añadimos
+        if (!playlist.getCollaborators().contains(newCollaborator)) {
+            playlist.getCollaborators().add(newCollaborator); // Se añade el nuevo colaborador
+        }
+
+        // Guardamos/actualizamos la BD
+        return playlistRepository.save(playlist);
+    }
+
+    /**
+     * Logica de negocio
+     * Eliminar colaborador por ID
+     * Solo el propietario (owner) podrá eliminar un colaborador
+     */
+
+    public Playlist removeCollaboratorWithId(Long playlistId, Long collaboratorId, Long ownerId){
+
+        // Obtener la playlist
+        Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(() -> new RuntimeException("Error: Playlist no encontrada"));
+
+        // Validar El dueño (owner) de la playlist
+        if (!playlist.getOwner().getId().equals(ownerId)) {
+            throw new RuntimeException("Error: Solo el propietario puede eliminar a los colaboradores");
+        }
+
+        // Validar que el usuario sea un colaborador
+        User collaborator = userRepository.findById(collaboratorId).orElseThrow(() -> new RuntimeException("Error: El usuario no existe o no es un colaborador"));
+
+        // validar que el colaborador forma parte de la playlist
+        if (!playlist.getCollaborators().contains(collaborator)) {
+            throw new RuntimeException("Error: El colaborador indicado no forma parte de la playlist");
+        }
+
+        //Eliminamos el colaborador y guardamos/actualizamos la BD
+        playlist.getCollaborators().remove(collaborator);
+        return playlistRepository.save(playlist);
+    }
+
+    /**
+     * Logica de negocio
+     * Eliminar colaborador por email
+     * Solo el propietario (owner) podrá eliminar un colaborador
+     */
+
+    public Playlist removeCollaboratorWithEmail(Long playlistId, String collaboratorEmail, String ownerEmail){
+
+        // Obtener la playlist
+        Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(() -> new RuntimeException("Error: Playlist no encontrada"));
+
+        // Validar El dueño (owner) de la playlist
+        if (!playlist.getOwner().getEmail().equals(ownerEmail)) {
+            throw new RuntimeException("Error: Solo el propietario puede eliminar a los colaboradores");
+        }
+
+        // Validar que el usuario sea un colaborador
+        User collaborator = userRepository.findByEmail(collaboratorEmail).orElseThrow(() -> new RuntimeException("Error: El usuario no existe o no es un colaborador"));
+
+        // validar que el colaborador forma parte de la playlist
+        if (!playlist.getCollaborators().contains(collaborator)) {
+            throw new RuntimeException("Error: El colaborador indicado no forma parte de la playlist");
+        }
+
+        //Eliminamos el colaborador y guardamos/actualizamos la BD
+        playlist.getCollaborators().remove(collaborator);
+        return playlistRepository.save(playlist);
     }
 
     /**
@@ -112,6 +204,30 @@ public class PlaylistService {
 
         // Añadimos la cancion a la playlist
         playlist.getSongs().add(song);
+
+        return playlistRepository.save(playlist);
+    }
+
+    /**
+     * Logica de negocio
+     * Eliminar una cancion a la playList
+     * Solo el dueño y los colaboradores podran eliminar canciones
+     */
+    public Playlist removeSongPlaylist (Long songId, Long playlistId, Long userId){
+
+        // Validamos que el usuario tenga permisos de edicion
+        if (!hasWritePermission(playlistId, userId)) {
+            throw new RuntimeException("Error: No tienes permisos de edicion");
+        }
+
+        // Playlist donde se eliminará la cancion
+        Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(() -> new RuntimeException("Error: Playlist no encontrada"));
+
+        // Validamos que la cancion existe y la guardamos
+        Song song = songRepository.findById(songId).orElseThrow(() -> new RuntimeException("Error: La cancion no existe"));
+
+        // Eliminamos la cancion a la playlist y guardamos/actualizamos la BD
+        playlist.getSongs().remove(song);
 
         return playlistRepository.save(playlist);
     }
