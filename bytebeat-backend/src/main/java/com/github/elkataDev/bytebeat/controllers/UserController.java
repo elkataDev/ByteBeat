b0e5785ac12bbe988fe93bdbe257808ad411669c
Antonio Espinola Ramirez <antonioespinola344@gmail.com>
2026-06-25 13:14:16 +0200

    Controlador UserController

--- bytebeat-backend/src/main/java/com/github/elkataDev/bytebeat/controllers/UserController.java
+++ bytebeat-backend/src/main/java/com/github/elkataDev/bytebeat/controllers/UserController.java
@@ -1,5 +1,30 @@
 package com.github.elkataDev.bytebeat.controllers;
 
-public class UserController  {
-    
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.elkataDev.bytebeat.model.Artist;
+import com.github.elkataDev.bytebeat.service.UserService;
+
+@RestController // Hace que devuelva JSON automáticamente
+@RequestMapping("/api/users") // La URL base
+public class UserController {
+
+    private final UserService userService; // Su conexión con el cerebro
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/{userId}/follow/{artistId}") // URL final: /api/users/5/follow/2
+    public ResponseEntity<?> followArtist(@PathVariable Long userId, @PathVariable Long artistId) {
+        // Llama al servicio 
+        Artist artist = userService.toggleArtistFollowWithId(userId, artistId);
+        
+        // Devuelve el JSON al navegador con un código 200 OK
+        return ResponseEntity.ok(artist); 
+    }
 }
